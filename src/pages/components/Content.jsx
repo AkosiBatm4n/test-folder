7394b48712809ccc7f7f678a4f4b82352adf8108
Ian Vincent L. Reyes <99298221+IanReyes2@users.noreply.github.com>
2024-11-10 17:23:06 +0800

    commit

---
+++ src/pages/components/Content.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Content = () => {
+  return (
+    <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20">
+      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Heroes of the skies</h1>
+        <div class="h-1 w-20 bg-red-500 rounded"></div>
+      </div>
+      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/superfortress.jpg" alt="content" width={1920} height={1275}/>
+          <h3 class="tracking-widest text-red-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/F-15E.jpg" alt="content" width={1200} height={815}/>
+          <h3 class="tracking-widest text-red-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
+          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      </div>
+    </div>
+</section>
+  )
+}
+
+export default Content
